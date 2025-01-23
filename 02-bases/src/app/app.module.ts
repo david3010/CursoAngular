36d8acf9bbd3ca300aadd2444f0c85d6e6e96ba1
David Gallegos <dgallegosv94@gmail.com>
2025-01-22 23:44:05 -0600

    Módulos en Angular

--- 02-bases/src/app/app.module.ts
+++ 02-bases/src/app/app.module.ts
@@ -1,22 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+
+// Modules
+import { CounterModule } from './counter/counter.module';
+import { HeroesModule } from './heroes/heroes.module';
+
 import { AppComponent } from './app.component';
-import { CounterComponent } from './counter/counter.component';
-import { ListComponent } from './heroes/list/list.component';
-import { HeroComponent } from './heroes/hero/hero.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    CounterComponent,
-    ListComponent,
-    HeroComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CounterModule,
+    HeroesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
