fb5ba758dea91090ded24308d03fd6d25863cfd9
David Gallegos <dgallegosv94@gmail.com>
2025-01-29 21:20:21 -0600

    Resolución de la tarea - AddCharacter

--- 02-bases/src/app/dbz/dbz.module.ts
+++ 02-bases/src/app/dbz/dbz.module.ts
@@ -3,10 +3,14 @@ import { CommonModule } from '@angular/common';
 
 // Import all the components that are part of the module
 import { MainPageComponent } from './pages/main-page.component';
+import { ListComponent } from './components/list/list.component';
+import { AddCharacterComponent } from './components/add-character/add-character.component';
 
 @NgModule({
   declarations: [
-    MainPageComponent
+    MainPageComponent,
+    ListComponent,
+    AddCharacterComponent
   ],
   exports: [
     MainPageComponent
