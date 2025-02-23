576a35118f8f94cc223e3108506fac6de6ecafc2
David Gallegos <dgallegosv94@gmail.com>
2025-02-22 20:38:02 -0600

    ViewChild - referencia al html

--- 03-gifs-app/src/app/gifs/components/search-bar/search-bar.component.ts
+++ 03-gifs-app/src/app/gifs/components/search-bar/search-bar.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+
+import { GifsService } from '../../services/gifs.service';
 
 @Component({
   selector: 'gifs-search-bar',
@@ -6,11 +8,25 @@ import { Component } from '@angular/core';
   template: `
     <h5>Search:</h5>
     <input
-    type="text"
-    placeholder="Search..."
-    class="form-control" />
+      type="text"
+      placeholder="Search..."
+      class="form-control"
+      (keyup.enter)="searchTag()"
+      #txtTagInput
+    />
   `,
 })
 export class SearchBoxComponent {
+  @ViewChild('txtTagInput')
+  txtTagInput!: ElementRef<HTMLInputElement>
 
+  constructor(private gifsService: GifsService) {}
+
+  searchTag() {
+    const newTag = this.txtTagInput.nativeElement.value
+
+    this.gifsService.searchTag(newTag)
+
+    this.txtTagInput.nativeElement.value = ''
+  }
 }
