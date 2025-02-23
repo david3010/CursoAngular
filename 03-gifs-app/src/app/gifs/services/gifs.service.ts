7a12e45523e8c5e545e470bc4eb790cc4a67d9ed
David Gallegos <dgallegosv94@gmail.com>
2025-02-23 16:47:15 -0600

    FH - Gifs Apps End

--- 03-gifs-app/src/app/gifs/services/gifs.service.ts
+++ 03-gifs-app/src/app/gifs/services/gifs.service.ts
@@ -1,20 +1,34 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class GifsService {
-
   private _tagsHistory: string[] = [];
+  private apiKey:       string = 'dGMJW12nlHSz46O3sy81BZFryoPshmUk';
+  private serviceUrl:   string = 'https://api.giphy.com/v1/gifs';
 
-  constructor() { }
+  constructor() {}
 
   get tagsHistory(): string[] {
     return [...this._tagsHistory];
   }
 
-  public searchTag(tag: string): void {
+  private organizeTagsHistory(tag: string): void {
+
+    tag.toLowerCase();
+
+    if(this._tagsHistory.includes(tag)) {
+      this._tagsHistory = this._tagsHistory.filter((tagHistory) => tagHistory !== tag);
+    }
+
     this._tagsHistory.unshift(tag);
+    this._tagsHistory = this._tagsHistory.splice(0, 10);
   }
 
+  public searchTag(tag: string): void {
+    if (tag.length === 0) return;
+
+    this.organizeTagsHistory(tag);
+  }
 }
